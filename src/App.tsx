5e62fde5ef771c5e23c0b21713a7beaf49c75101
Thanh Nguyen <thanhnx.605@gmail.com>
2020-09-07 20:57:26 +0100

    Reinit with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = (): React.FC => {
+type Props = {
+  content: string;
+};
+
+const App: React.FC<Props> = ({ content }) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +20,7 @@ const App = (): React.FC => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with some {content}
         </a>
       </header>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App content="content" />
   </React.StrictMode>,
   document.getElementById('root')
 );
