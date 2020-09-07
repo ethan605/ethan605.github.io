082d0efc00b2e1565015c4e3141db0ee017c3f9e
Thanh Nguyen <thanhnx.605@gmail.com>
2020-09-07 22:35:50 +0100

    Add webfontloader & load Noto Sans

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,18 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Noto Sans:400'],
+  },
+});
 
 const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    font-family: 'Noto Sans', 'Segoe UI', 'Roboto', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 
   code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
   }
 `;
 
