806c95a8ea726085e7712a1e0ac170db356d92c5
Thanh Nguyen <thanhnx.605@gmail.com>
2020-09-10 23:22:03 +0100

    Refine global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -4,7 +4,7 @@ import { normalize } from 'styled-normalize';
 
 WebFont.load({
   google: {
-    families: ['Noto Sans', 'Source Code Pro'],
+    families: ['Oxygen', 'Fira Code'],
   },
 });
 
@@ -12,14 +12,23 @@ const GlobalStyles = createGlobalStyle`
   ${normalize}
 
   :root {
-    font-family: 'Noto Sans', 'Segoe UI', 'Roboto', 'Ubuntu', sans-serif;
-    font-size: 16px;
+    font-family: 'Oxygen', 'PT Sans', 'Segoe UI', 'Ubuntu', sans-serif;
+    font-size: 14px;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 
+  html, body, h1, h2, h3, h4, h5, h6, ul, li {
+    margin: 0
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
   code, #resume {
-    font-family: 'Source Code Pro', Menlo, Monaco, Consolas, monospace;
+    font-family: 'Fira Code', Menlo, Monaco, Consolas, monospace;
   }
 `;
 
