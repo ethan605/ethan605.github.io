cdb22229f1a81d684359b9afb259aed602072223
Thanh Nguyen <thanhnx.605@gmail.com>
2020-09-11 12:13:15 +0100

    Add dark theme & customise

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -3,6 +3,9 @@ import { DecorationTypes } from '../types/resume';
 export type PaperSize = 'A3' | 'A4' | 'A5';
 
 export type ColorScheme = {
+  background: string;
+  forground: string;
+  url: string;
   prompts: {
     item: string;
   };
@@ -41,6 +44,22 @@ export type ResumeTheme = {
 } & TerminalScheme;
 
 const colorSchemeLight: ColorScheme = {
+  background: '',
+  forground: '',
+  url: '',
+  prompts: {
+    item: 'rgb(220, 220, 220)',
+  },
+};
+
+/**
+ * Inspired by pure-prompt & snazzy theme
+ * @see https://github.com/sindresorhus/pure/blob/0a3801807dc274515dd6a2a26d56f63881ad07b4/pure.zsh#L772
+ */
+const colorSchemeDark: ColorScheme = {
+  background: '',
+  forground: '',
+  url: '',
   prompts: {
     item: 'rgb(220, 220, 220)',
   },
@@ -80,6 +99,12 @@ const terminalScheme: TerminalScheme = {
 
 export const light: ResumeTheme = {
   colors: colorSchemeLight,
+  page: pageSettings,
+  ...terminalScheme,
+};
+
+export const dark: ResumeTheme = {
+  colors: colorSchemeDark,
   page: pageSettings,
   ...terminalScheme,
 };
