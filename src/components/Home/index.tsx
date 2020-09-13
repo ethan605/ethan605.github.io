f3844de9c78a9fef936330dd9962e1db5fd0fb7d
Thanh Nguyen <thanhnx.605@gmail.com>
2020-09-14 00:00:34 +0100

    Draft Home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Home: React.FC = () => (
-  <div>
-    <Link to="/">Home</Link>
-    <Link to="/resume">Resume</Link>
-  </div>
-);
+const Container = styled.div`
+  align-items: stretch;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  height: 50vh;
+  margin: 25vh auto;
+  min-width: 50rem;
+  min-height: 30rem;
+  width: 50vw;
+  overflow: hidden;
+`;
+
+const TopBar = styled.div`
+  background-color: #26282a;
+  border-bottom: 1px solid black;
+  height: 2rem;
+`;
+
+const Content = styled.div`
+  align-items: center;
+  background-color: #282a36;
+  color: #eff0eb;
+  display: flex;
+  justify-content: space-evenly;
+  flex: 1;
+`;
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <TopBar />
+      <Content>
+        <Link to="/">Home</Link>
+        <Link to="/resume">Resume</Link>
+      </Content>
+    </Container>
+  );
+};
 
 export default Home;
