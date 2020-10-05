ce5bac08b15ed6a7e68dccae087c89496da07eb9
Thanh Nguyen <thanhnx.605@gmail.com>
2020-10-05 11:11:45 +0100

    Add sampleArr util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,12 @@ import MD5 from 'md5.js';
 
 export const buildIteratorKey = (content: string): string =>
   new MD5().update(content).digest('hex');
+
+export const sampleArr = <T>(arr: T[]): T | null => {
+  if (!arr.length) {
+    return null;
+  }
+
+  const randomIdx = Math.floor(Math.random() * arr.length);
+  return arr[randomIdx];
+};
