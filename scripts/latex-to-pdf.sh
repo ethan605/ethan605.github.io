5c648dfeb6c50262dafa59b403a2862088333d54
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-02-20 13:41:52 +0000

    Add scripts/latex-to-pdf & update public resumes

---
+++ scripts/latex-to-pdf.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+PREFIX="Thanh-Ethan-Nguyen-resume"
+VARIANTS=(dark light)
+
+function main() {
+  local version=${1-}
+
+  if [[ -z "$version" ]]; then
+    echo "Missing version"
+    return 1
+  fi
+
+  cd resumes/
+
+  local tex_version_matching="VERSION \\\normalsize\\\texttt"
+  mv content.tex{,.bak}
+  cat content.tex.bak | sed "s,$tex_version_matching{.*,$tex_version_matching{$version}}," >| content.tex
+
+  for variant in ${VARIANTS[@]}; do
+    local jobname="$PREFIX-$version-$variant"
+    pdflatex -output-directory=../public -jobname="$jobname" "$variant.tex"
+  done
+}
+
+main "$@"
