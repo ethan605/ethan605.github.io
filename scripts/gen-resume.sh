660edd62114a48e05925798a0258a19b3e6b2534
Ethan Nguyen <ethan.nguyen@onfido.com>
2023-05-11 10:02:33 +0100

    Refactor Version

--- scripts/gen-resume.sh
+++ scripts/gen-resume.sh
@@ -5,15 +5,9 @@ PREFIX="Thanh-Ethan-Nguyen-resume"
 
 function main() {
   local version
-  version=$(jq -r '.resume_version' package.json)
-
-  if [[ -z "$version" ]]; then
-    echo "Missing version"
-    return 1
-  fi
 
   cd resume/
-  echo "$version" > version.tex
+  version=$(cat version.tex)
 
   local output="$PREFIX-$version"
   docker compose up resume
